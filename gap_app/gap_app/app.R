5a99f88ac51e07f6318334064aafee5f2333b105
Madi Harris <maharris@bren.ucsb.edu>
2018-02-26 11:49:23 -0800

    app script

--- gap_app/gap_app/app.R
+++ gap_app/gap_app/app.R
@@ -8,36 +8,36 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("West Coast Ocean Acidification Monitoring Inventory: Assets and Gaps"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        
-      ),
+  
+  # Application title
+  titlePanel("West Coast Ocean Acidification Monitoring Inventory: Assets and Gaps"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
       
-      # Show a map of monitoring gaps (raster)
-      mainPanel(
-         plotOutput("gaps")
-      )
-   )
+    ),
+    
+    # Show a map of monitoring gaps (raster)
+    mainPanel(
+      plotOutput("gaps")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$gap_map <- renderLeaflet({
-     
-     leaflet() %>% 
-       addTiles() %>%
-       addProviderTiles('Esri.OceanBasemap') %>% 
-       addRasterImage(gaps, colors = pal) %>% 
-       addLegend(
-         pal = pal, values = values(gaps),
-         title = "Monitoring Gaps")
-   })
+  
+  output$gap_map <- renderLeaflet({
+    
+    leaflet() %>% 
+      addTiles() %>%
+      addProviderTiles('Esri.OceanBasemap') %>% 
+      addRasterImage(gaps, colors = pal) %>% 
+      addLegend(
+        pal = pal, values = values(gaps),
+        title = "Monitoring Gaps")
+  })
 }
 
 # Run the application 
