2f25d25976e11d97cda10ac33e89f0814b03e3f2
Madi Harris <maharris@bren.ucsb.edu>
2018-02-26 11:41:26 -0800

    edits to app script

--- gap_app/gap_app/app.R
+++ gap_app/gap_app/app.R
@@ -1,11 +1,8 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+########################
+#ESM 244 Final Project
+#Courtney Cochran and Madi Harris
+#OA Monitoring Inventory Gaps Decision Tool
+########################
 
 library(shiny)
 
@@ -18,16 +15,12 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        
       ),
       
-      # Show a plot of the generated distribution
+      # Show a map of monitoring gaps (raster)
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("gaps")
       )
    )
 )
@@ -35,13 +28,15 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$gap_map <- renderLeaflet({
+     
+     leaflet() %>% 
+       addTiles() %>%
+       addProviderTiles('Esri.OceanBasemap') %>% 
+       addRasterImage(gaps, colors = pal) %>% 
+       addLegend(
+         pal = pal, values = values(gaps),
+         title = "Monitoring Gaps")
    })
 }
 
