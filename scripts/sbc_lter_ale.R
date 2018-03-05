f70719897ad7e70a798f4b182b35036444eb5f3b
Courtney Thomas <clthomas07@gmail.com>
2018-03-04 17:23:46 -0800

    Adding script for ALE ts

---
+++ scripts/sbc_lter_ale.R
@@ -0,0 +1,50 @@
+########### SBC LTER ALE (Alegria Reef) Data Exploration
+
+if (!require(pacman)) install.packages("pacman")
+library(pacman)
+p_load(
+  here,
+  plyr,
+  tidyr,
+  dplyr,
+  readr,
+  ggplot2,
+  extrafont,
+  scales)
+
+# import fonts to use 'spectral' in graphics
+# note 'spectral' fonts must be installed on the computer - have not found a workaround for importing from github yet
+font_import()
+
+# read ALE_arag CSV from github
+ale <- read_csv("data/ALE_arag.csv")
+
+# set -9999 values to NA
+ale[ale==-9999] <- NA
+
+colnames(ale) <- c("date.time", "site", "lat", "long", "aragonite")
+
+# separate out date and time
+ale <- separate(ale, "date.time", c("date","time"), sep=" ")
+ale$date <- as.Date(ale$date,'%Y-%m-%d')
+
+# remove NA for mean/std deviation calculations
+ale.wo.na <- na.omit(ale)
+
+png(filename="visualizations/ale.arag.ts.png", width = 10, height = 7, units = 'in', res = 300)
+ggplot(data=ale, aes(date, aragonite)) +
+  geom_rect(data=ale, xmin=-Inf,
+            xmax=Inf,
+            ymin=(mean(ale.wo.na$aragonite) - sd(ale.wo.na$aragonite)),
+            ymax=(mean(ale.wo.na$aragonite) + sd(ale.wo.na$aragonite)), aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),
+            fill="grey90",alpha=0.5) +
+  geom_line(colour = "grey30", size = 0.4, alpha = 0.8) +
+  geom_hline(yintercept = mean(ale.wo.na$aragonite), color="dodgerblue", linetype = "dashed") +
+  labs(title="Santa Barbara Channel Aragonite Saturation State\n",x="\nDate", y = "Aragonite Saturation State\n") +
+  theme_classic() +
+  theme(text=element_text(family="Spectral"), plot.title = element_text(size = 20, hjust = 0.5, face = "bold"), axis.title = element_text(size = 18), axis.text = element_text(size = 16)) +
+  scale_y_continuous(limits = c(0, 4.25)) +
+  scale_x_date(date_breaks = "3 months", labels = date_format("%b-%y"))
+dev.off()
+
+#annotate(geom = "text", x = as.Date("2012-02-03"), y = 3.5,label= "Mean = 2.327\nSD = 0.364",fontface = "bold") +
