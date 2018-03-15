5d7d3cc1a9bb759baa50d9bb34128eb70f8abb7e
cacochran <cac@ucsb.edu>
2018-03-14 22:09:49 -0700

    Got rid of trash code and added descriptions to code

--- gap_app/gap_app/app.R
+++ gap_app/gap_app/app.R
@@ -6,68 +6,27 @@
 
 library(shiny)
 library(leaflet)
+#Convert gap analysis data layers to rasters 
 fullinventory <- raster("full_invetory_gaps.tif")
 carbonatecomplete <- raster("carbcomplete_gaps.tif")
 highfreq <- raster("highfreq_gaps.tif")
 
-fullinventory
+#Colors for displaying gap analysis 
 pal <- colorRampPalette(c("#0C2C84", "#41B6C4", "#FFFFCC"))
 
-fullinvetorygaps <- leaflet() %>%
-  addTiles() %>%
-  addProviderTiles('Esri.OceanBasemap') %>% 
-  addRasterImage(fullinventory)
+#Load inventory points and convert to spatial points dataframe
+oahfocus <- read_csv("oahfocus.csv")
+coordinates(oahfocus) <-~Longitude+Latitude
 
-#load oah focus csv 
-#coordinates(oahfocus) <-~Longitude+Latitude
-class(oahfocus)
-
-leaflet(data = oahfocus) %>% 
-  addTiles() %>%
-  addProviderTiles('Esri.OceanBasemap') %>% 
-  addCircleMarkers(
-    radius = 3, 
-    color = "black",
-    #popup = ~as.character(), label = ~as.character()
-    stroke = FALSE, fillOpacity = 0.5) 
-
-
-#addLegend(
-  #pal = pal, values = values(gaps),
-  #title = "Monitoring Gaps")
-
-
-#selectInput("gaptype", "Type of Monitoring Gap:",
-           # choices= c("Generic Gaps" = "fullinventory", "Carbonate Complete Gaps" = #"carbonatecomplete", "High Frequency Gaps" = "highfreq"))
-
+#Create other subsets of inventory points
 carbpoints<-subset(oahfocus, DisCrbPmtr>1 | ISCrbPmtr > 1)
 highfreqpoints<-subset(oahfocus, MeasFreq > 364)
 
-
-#WishList
-#for pop-ups, assettype (mooring vs cruise)
-#legends - CC - rae has code for colors by percentages
-#fix colors - CC
-#add text box with data description
-#map title?
-
+#Create Color Palette for Inventory Points
 asset_pal <- colorFactor(topo.colors(7), oahfocus$AssetType)
 oahfocus$AssetID <- as.factor(oahfocus$AssetID)
 unique(oahfocus$AssetType)
 class(oahfocus$AssetID)
-
-leaflet(oahfocus) %>% 
-  addTiles() %>%
-  addProviderTiles('Esri.OceanBasemap') %>% 
-  addCircleMarkers(
-    radius = 4, 
-    
-    color = asset_pal,
-    popup = ~as.character(ProjectID), 
-    stroke = FALSE, fillOpacity = 0.5) 
-
-
-
 
 
 #########################################################################################
@@ -78,16 +37,20 @@ ui <- fluidPage(
   # Application title
   titlePanel("West Coast Ocean Acidification Monitoring Inventory: Assets and Gaps"),
   
+  #Introduction with background information
   fluidRow(column(width=3),
            p(" "),
            column(width=12, offset=3),
            h4("Introduction"),
            p("The West Coast Ocean Acidification and Hypoxia (OAH) Monitoring Inventory is a joint project of the Pacific Coast Collaborative and the Interagency Working Group on Ocean Acidification (OA). It emerged as a project to identify and catalog all monitoring assets along the West Coast that are measuring metrics of OAH. The assets are owned and operated with different priorities by different institutions, but are brought together in an attempt to analyze how well we understand and are monitoring OA trends at a regional level."),
+           
+           #Description of data displayed in first output
            h4("The Monitoring Inventory"),
           p("
 The monitoring network consists of various assets that come in the form of moorings, buoys, cruises, and other sensors - each with various aspects of data quality (temporal frequency of measurements and quality of data). An ideal monitoring network would have high frequency of data collection and also be 'carbonate complete' meaning that aragonite saturation state can be calculated.") 
            ), 
   
+  #Create radio buttons to select subset of inventory points to display
   sidebarLayout(
     sidebarPanel(
       radioButtons("pointtype", "Select:", 
@@ -100,10 +63,12 @@ The monitoring network consists of various assets that come in the form of moori
     
   ),
   
+  #Description of data in second output
   fluidRow(column(width=12),
            h4("Gap Analysis"),
            p("A main recommendation of the West Coast OAH Panel is that a gap analysis be conducted on the combined total monitoring network. This is crucial for identifying areas of future research and priorities for expanding the network. This gap analysis shows gaps across the entire network, gaps in carbonate complete monitoring, and gaps in high frequency monitoring.")),
   
+  #Create dropdown menu to select which type of gaps to display
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
@@ -126,6 +91,7 @@ The monitoring network consists of various assets that come in the form of moori
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  #Map displaying monitoring assets
   output$point_map <- renderLeaflet({
     inventorypoints <- switch(input$pointtype,
                               "All Assets" = oahfocus,
@@ -149,16 +115,11 @@ server <- function(input, output) {
     
   })
   
-  #maps <- reactiveValues()
+ 
   
-  #maps$full <- fullinventory
-  #maps$carb <- carbonatecomplete
-  #maps$high <- highfreq
-  
-  #gaprasters <- (c(fullinventory, carbonatecomplete, highfreq) == input$gaptype)
+  #Map displaying gaps in monitoring netwokr
   output$gap_map <- renderLeaflet({
     
-    #gappal1 <- palette(c("slateblue4", "slateblue", "plum", "orangered2"))
     gappal <- colorRampPalette(c("slateblue4", "slateblue", "plum", "orangered2"))
     
    gaprasters <- switch(input$gaptype, 
@@ -172,7 +133,6 @@ server <- function(input, output) {
       addRasterImage(gaprasters, colors = gappal(4)) %>% 
       addLegend("topright", colors=gappal(4), values= values(gaprasters), labels = c("Sufficient Data", "Low Priority Gaps", "High Priority Gaps", "Severe Gaps"), title = "Severity of Gap")
       
-    #"topright", pal=gappal, values= values(gaprasters), labels = c("Sufficient Data", "Low #Priority Gaps", "High Priority Gaps", "Severe Gaps")
   })
   
 
